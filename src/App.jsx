83e9dac0560626d678e50e60b67145fdccaa989f
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-03 15:33:55 +0200

    Add footer, navbar, and context provider; edit routes and create new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CountContext from './contexts/CountContext';
 //layout
 import DefaultLayout from './layout/DefaultLayout'
 //pagine
 import Home from './pages/Home'
-import Post from './pages/Post'
+import Post from './pages/PostCard'
 import Product from './pages/Product'
 import PostsList from './pages/PostsList'
 import NotFound from './pages/NotFound'
@@ -12,18 +13,18 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/postslist" element={<PostsList />} />
-          <Route path="/products/:id" element={<Product />} />
-          <Route path="/chisiamo" element={<ChiSiamo />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CountContext.Provider value={{ count: 1 }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/postslist" element={<PostsList />} />
+            <Route path="/ostslist/:id" element={<PostCard />} />
+            <Route path="/*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CountContext.Provider>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+
+      <div className="navbar">
+          <div className="menu-sx">
+            <img className="logo" src="/public/vite.svg" alt="" />
+          </div>
+          <ul className="menu-dx">
+              <li><NavLink className="nav-link" to={"/"}>Home</NavLink></li>
+              <li><NavLink className="nav-link" to={"/PostsList"}>Posts</NavLink></li>
+              <li><NavLink className="nav-link" to={"/PostCard"}>Post card</NavLink></li>
+          </ul>
+      </div>
+    );
+  } 
